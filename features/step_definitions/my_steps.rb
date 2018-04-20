8cfe2cb78805fc9ec935e8f965376320a5f8e47c
kleer <kleer@kleer-csd-box>
2018-04-20 10:04:42 -0500

    prueba steps

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -2,10 +2,6 @@ Given(/^que abri el juego$/) do
   visit '/'
 end
 
-Then(/^debo ver "([^"]*)"$/) do |arg1|
-  "Bienvenido a Parejas Mundialistas"
-end
-
 Cuando(/^selecciono el boton iniciar$/) do
  click_button("Iniciar") 
 end
