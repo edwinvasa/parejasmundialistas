237fc9d3f54ba9a69ecf45a10952293ec120a5f5
kleer <kleer@kleer-csd-box>
2018-04-20 09:59:36 -0500

    form 1 y 2 integrados

--- app.rb
+++ app.rb
@@ -4,3 +4,7 @@ require './config'
 get '/' do
 	erb :index
 end
+
+post '/selecciona_pareja' do
+	erb :selecciona_pareja
+end
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,14 +6,8 @@ Then(/^debo ver "([^"]*)"$/) do |arg1|
   "Bienvenido a Parejas Mundialistas"
 end
 
-
-Dado(/^que abri el juego$/) do
-  #visit '/'
-end
-
 Cuando(/^selecciono el boton iniciar$/) do
-# click_button("Iniciar") 
-visit '/selecciona_pareja.erb'
+ click_button("Iniciar") 
 end
 
 Entonces(/^debo ver "([^"]*)"$/) do |texto|
